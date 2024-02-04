7c89619203874554f302a02795e88688d26d98f3
baekseungchan1117 <bsc7386@gmail.com>
2024-02-04 19:50:05 +0900

    2단계 해결

--- src/test/java/com/example/sbb/AnswerRepository.java
+++ src/test/java/com/example/sbb/AnswerRepository.java
@@ -1,7 +1,6 @@
 package com.example.sbb;
 
 import com.example.sbb.answer.Answer;
-import com.example.sbb.answer.AnswerRepository;
 import com.example.sbb.question.Question;
 import com.example.sbb.question.QuestionRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,13 +11,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.time.LocalDateTime;
 
 @SpringBootTest
-public class AnswerApplicationTest {
+public class AnswerRepository {
 
     @Autowired
     private QuestionRepository questionRepository;
 
     @Autowired
-    private AnswerRepository answerRepository;
+    private com.example.sbb.answer.AnswerRepository answerRepository;
 
     private static int lastSampleDateId;
 
@@ -36,7 +35,20 @@ public class AnswerApplicationTest {
     }
 
     private void clearSampleData() {
+        // question을 만든다.
+        Question q1 = new Question();
+        q1.setSubject("sbb가 무엇인가요?");
+        q1.setContent("sbb에 대해서 알고 싶습니다.");
+        q1.setCreateDate(LocalDateTime.now());
 
+        questionRepository.save(q1);
+
+        Question q2 = new Question();
+        q2.setSubject("스프링부트 모델 질문입니다.");
+        q2.setContent("id는 자동으로 생성되나요?");
+        q2.setCreateDate(LocalDateTime.now());
+
+        this.questionRepository.save(q2);  // 두번쨰 질문 저장
 
     }
 
